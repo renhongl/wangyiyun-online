c569cb7db2fe83042599caf312b13ccf107add98
renhongl <1075220132@qq.com>
2019-11-01 17:08:15 +0800

    update

--- desktop/index.js
+++ desktop/index.js
@@ -1,15 +1,22 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 app.commandLine.appendSwitch ("disable-http-cache");
 
 let win;
 
+ipcMain.on('close', (event, arg) => {
+  app.quit();
+});
+
 app.on('closed', () => {
-  win = null
+  win = null;
 });
 
 app.on('ready', function() {
-    win = new BrowserWindow({ width: 1000, height: 800, autoHideMenuBar: false, frame: false });
+    win = new BrowserWindow({ width: 1000, height: 800, autoHideMenuBar: false, frame: false, webPreferences: {
+      nodeIntegration: true,
+    } });
     win.loadURL('https://renhongl.github.io/wangyiyun-online/');
+    // win.loadURL('http://localhost:4200');
     win.setMenu(null);
 });
